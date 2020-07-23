a501e400fd938d0fffe88467d3b9434a611440db
Andrew-J-Williams <1williamsaj@gmail.com>
2020-07-23 17:18:31 -0500

    removed 'status: 200' from User controller

--- eg-backend/app/controllers/api/v1/users_controller.rb
+++ eg-backend/app/controllers/api/v1/users_controller.rb
@@ -3,19 +3,19 @@ class Api::V1::UsersController < ApplicationController
     def index 
         @users = User.all
 
-        render json: @users, status: 200
+        render json: @users
     end
 
     def show
         @user = User.find(user_params[:id])
 
-        render json: @user, status: 200
+        render json: @user
     end
 
     def create
-        @user = User.find_or_create_by(username: user_params[:username])
+        @user = User.create(user_params)
 
-        render json: @user, status 200
+        render json: @user
     end
 
     private
