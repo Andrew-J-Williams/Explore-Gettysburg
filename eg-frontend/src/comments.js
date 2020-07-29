d44c8b6d5dca1c3db5346563d97fbb3593c091a2
Andrew-J-Williams <1williamsaj@gmail.com>
2020-07-29 18:52:40 -0500

    creating divs for all comments and new comments

--- eg-frontend/src/comments.js
+++ eg-frontend/src/comments.js
@@ -3,11 +3,23 @@ function fetchComments(markerUrl, eventId){
         .then(response => response.json())
         .then(data => {
             const commentContainer = document.querySelector("#comment-container")
+            const scrollSection = document.createElement('div')
+            const newComment = document.createElement('div')
+            scrollSection.classList.add("scroll-container")
+            scrollSection.id = "scroll-container"
+            newComment.classList.add("new-comment-container")
+            newComment.id = "new-comment-container"
 
-            commentContainer.innerHTML = `
-                <h2>THIS IS WHERE COMMENTS GO</h2>
-                <p class="info-text">${eventId}</p>
-                <p class="info-text"></p>
+            commentContainer.append(scrollSection)
+            commentContainer.append(newComment)
+
+            scrollSection.innerHTML = `
+                <h2> This section will scroll through comments </h2>
             `
+
+            newComment.innerHTML = `
+                <h2> This section will create new comments </h2>
+            `
+
         });
 }
