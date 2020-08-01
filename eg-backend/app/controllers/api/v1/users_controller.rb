c9a7f339d120d827dc90c3477cab67baacb4dca6
Andrew-J-Williams <1williamsaj@gmail.com>
2020-08-01 15:19:12 -0500

    created user folder, constructor

--- eg-backend/app/controllers/api/v1/users_controller.rb
+++ eg-backend/app/controllers/api/v1/users_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def create
-        @user = User.create(user_params)
+        @user = User.find_or_create_by(username: user_params[:username])
 
         render json: @user
     end
@@ -21,6 +21,6 @@ class Api::V1::UsersController < ApplicationController
     private
 
     def user_params
-      params.require(:user).permit(:id, :username)
+      params.require(:user).permit(:id, :username, :password)
     end
 end
---
+++ eg-frontend/src/users.js
@@ -0,0 +1,13 @@
+class User{
+    constructor(name, password){
+    this.username = name
+    this.password = password
+    }
+}
+
+function createUser(username, password){
+
+
+
+    
+}
