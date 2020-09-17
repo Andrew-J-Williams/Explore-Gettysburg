342879c12b110844ee04a5cd7a420d8b0113439b
Andrew-J-Williams <1williamsaj@gmail.com>
2020-09-16 21:22:36 -0500

    Refactored code for events.js, removed hardcoding

--- eg-frontend/src/events.js
+++ eg-frontend/src/events.js
@@ -29,46 +29,83 @@
             const descriptP = document.createElement('p')
             const hidden = document.createElement('p')
 
-            //infoContainer.innerHTML = `
-            //<br>
-            //<br>
-            //<div class="total">
-            //<div class="event-title"> 
-            //    <h2>${data.name}</h2>
-            //</div>
-            //<br>
-            //<div class="date-box">
-            //    <h3>Date: </h3>
-            //    <p><i>${data.date}</i></p>
-            //</div>
-           //<div class="box">
-           //     <div class="left-side">
-           //         <h4>Union General: </h4>
-           //         <p><i>${data.union_leader}</i></p>
-           //     </div>
-           //     <br><br>
-           //     <div class="right-side">
-           //         <h4>Confederate General: </h4>
-           //         <p><i>${data.confederate_leader}</i></p>
-           //     </div>
-           // </div>
-           // <div class="box">
-           //     <div class="left-side">
-           //         <h4>Union Army: </h4>
-           //         <p><i>${data.union_army}</i></p>
-           //     </div>
-           //     <div class="right-side">
-           //        <h4>Confederate Army: </h4>
-           //         <p><i>${data.confederate_army}</i></p>
-           //     </div>
-           // </div>
-           // <br>
-           // <img src=${data.url} class="event-image">
-           // </div>
-           // <div class="description-section">
-           //     <p class="info-text">${data.description}</p>
-           // </div>
-           // <p id="hidden-event-id" class="hidden-event-id">${data.id}</p>
-           // `
+            total.classList.add("total")
+            eventTitle.classList.add("event-title")
+            dateBox.classList.add("date-box")
+            boxOne.classList.add("box")
+            boxTwo.classList.add("box")
+            leftSideOne.classList.add("left-side")
+            leftSideTwo.classList.add("left-side")
+            rightSideOne.classList.add("right-side")
+            rightSideTwo.classList.add("right-side")
+            img.classList.add("event-image")
+            hidden.classList.add("hidden-event-id")
+            descriptSection.classList.add("description-section")
+            descriptP.classList.add("info-text")
+            hidden.id = "hidden-event-id"
+
+            name.innerText = `${data.name}`
+            date.innerText = `${data.date}`
+            dateP.innerText = `${data.date}`
+            dateP.style = "italic"
+            unionGeneral.innerText = `Union General: `
+            unionGenP.innerText = `${data.union_leader}`
+            unionGenP.style = "italic"
+            confedGeneral.innerText = `Confederate General: `
+            confedGenP.innerText = `${data.confederate_leader}`
+            confedGenP.style = "italic"
+            unionArmy.innerText = `Union Army: `
+            unionArmyP.innerText = `${data.union_army}`
+            unionArmyP.style = "italic"
+            confedArmy.innerText = `Confederate Army: `
+            confedArmyP.innerText = `${data.confederate_army}`
+            confedArmyP.style = "italic"
+            img.src = `${data.url}`
+            descriptP.innerText = `${data.description}`
+            hidden.innerText = `${data.id}`
+
+            eventTitle.append(name)
+
+            dateBox.append(date)
+            dateBox.append(dateP)
+
+            descriptSection.append(descriptP)
+
+            leftSideOne.append(unionGeneral)
+            leftSideOne.append(unionGenP)
+
+            rightSideOne.append(confedGeneral)
+            rightSideOne.append(confedGenP)
+
+            leftSideTwo.append(unionArmy)
+            leftSideTwo.append(unionArmyP)
+
+            rightSideTwo.append(confedArmy)
+            rightSideTwo.append(confedArmyP)
+
+            boxOne.append(leftSideOne)
+            boxOne.append(br)
+            boxOne.append(br.cloneNode())
+            boxOne.append(rightSideOne)
+
+            boxTwo.append(leftSideTwo)
+            boxTwo.append(br.cloneNode())
+            boxTwo.append(br.cloneNode())
+            boxTwo.append(rightSideTwo)
+
+            total.append(eventTitle)
+            total.append(br.cloneNode())
+            total.append(dateBox)
+            total.append(boxOne)
+            total.append(boxTwo)
+            total.append(br.cloneNode())
+            total.append(img)
+
+            infoContainer.append(br.cloneNode())
+            infoContainer.append(br.cloneNode())
+            infoContainer.append(total)
+            infoContainer.append(descriptSection)
+            infoContainer.append(hidden)
+
             });
     }
